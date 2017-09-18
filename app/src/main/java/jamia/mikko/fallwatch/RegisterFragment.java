9c26e77b2e4ee36c3c78289e438ca1342f07612a
jamiamikko <jamiamikko@gmail.com>
2017-09-18 14:44:08 +0300

    Change RegisterActivity to use fragments

---
+++ app/src/main/java/jamia/mikko/fallwatch/RegisterFragment.java
@@ -0,0 +1,52 @@
+package jamia.mikko.fallwatch;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+
+public class RegisterFragment extends Fragment {
+
+    private EditText userName, contact1, contact2;
+
+    public RegisterFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_register, container, false);
+
+        return v;
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        userName = (EditText) getActivity().findViewById(R.id.yourNameEdit);
+        contact1 = (EditText) getActivity().findViewById(R.id.firstContactEdit);
+        contact2 = (EditText) getActivity().findViewById(R.id.secondContactEdit);
+
+        contact1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                ReadContactsFragment fragment = new ReadContactsFragment();
+
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, fragment)
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+    }
+}
