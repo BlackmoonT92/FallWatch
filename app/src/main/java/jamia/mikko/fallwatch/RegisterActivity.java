5bd3bd03fbc0c9bbdfe0f7b9a11735dd046eae15
jamiamikko <jamiamikko@gmail.com>
2017-09-18 13:17:00 +0300

    RegisterActivity layout

--- app/src/main/java/jamia/mikko/fallwatch/RegisterActivity.java
+++ app/src/main/java/jamia/mikko/fallwatch/RegisterActivity.java
@@ -3,11 +3,14 @@ package jamia.mikko.fallwatch;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_register);
+
     }
+
 }
