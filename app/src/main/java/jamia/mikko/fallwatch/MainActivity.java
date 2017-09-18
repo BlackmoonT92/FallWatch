70a9b7f5f98e657ad2f61fd032102fe8507ee9bd
jamiamikko <jamiamikko@gmail.com>
2017-09-18 13:36:39 +0300

    Start app with RegisterActivity

--- app/src/main/java/jamia/mikko/fallwatch/MainActivity.java
+++ app/src/main/java/jamia/mikko/fallwatch/MainActivity.java
@@ -1,13 +1,28 @@
 package jamia.mikko.fallwatch;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private int permissionReadContactsKey = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_main);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, permissionReadContactsKey);
+
+        }
+
+        Intent registerIntent = new Intent(this, RegisterActivity.class);
+        startActivity(registerIntent);
     }
 }
--- app/src/main/java/jamia/mikko/fallwatch/RegisterActivity.java
+++ app/src/main/java/jamia/mikko/fallwatch/RegisterActivity.java
@@ -1,18 +1,24 @@
 package jamia.mikko.fallwatch;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
-
+    private EditText userName, contact1, contact2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_register);
-
+        
     }
 
 }
