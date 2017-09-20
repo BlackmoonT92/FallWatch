3f8b6c2f046239bf8d4a10ee795b0fea0924687e
Roope Vilo <rrvilo@gmail.com>
2017-09-20 12:27:14 +0300

    tracking status switch implemented

--- app/src/main/java/jamia/mikko/fallwatch/HomeFragment.java
+++ app/src/main/java/jamia/mikko/fallwatch/HomeFragment.java
@@ -1,18 +1,19 @@
 package jamia.mikko.fallwatch;
 
-import android.net.Uri;
+import android.graphics.drawable.Animatable;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.FrameLayout;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Switch;
 import android.widget.ViewSwitcher;
 
 /**
@@ -23,7 +24,8 @@ public class HomeFragment extends Fragment {
 
     private ImageSwitcher imgSwitcher;
     private Button onOff;
-    private boolean isOn;
+    private ImageView statusOn;
+    private ImageView statusOff;
 
     public HomeFragment(){}
 
@@ -41,7 +43,30 @@ public class HomeFragment extends Fragment {
 
         getActivity().setTitle("Home");
 
-        Log.i("DEBUG", "Home fragment");
+        statusOn = (ImageView) view.findViewById(R.id.status_on);
+        statusOff = (ImageView) view.findViewById(R.id.status_off);
+
+        Switch trackerSwitch = (Switch) view.findViewById(R.id.tracking_switch);
+        //final ImageView statusImg = (ImageView) view.findViewById(R.id.image_tracking_status);
+        trackerSwitch.setChecked(false);
+
+        trackerSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+
+                if(isChecked) {
+                    Log.i("DEBUG", "Switch on");
+                    ((Animatable) statusOn.getDrawable()).start();
+                    statusOff.setVisibility(View.INVISIBLE);
+                    statusOn.setVisibility(View.VISIBLE);
+                }else {
+                    statusOff.getDrawable();
+                    Log.i("DEBUG", "Switch off");
+                    statusOn.setVisibility(View.INVISIBLE);
+                    statusOff.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
         return view;
     }
