818c7d8384fb4e9608cf705881c9424b30d07ccc
jamiamikko <jamiamikko@gmail.com>
2017-10-07 12:47:54 +0300

    Clean and comment Constants

--- app/src/main/java/jamia/mikko/fallwatch/Detection/Constants.java
+++ app/src/main/java/jamia/mikko/fallwatch/Detection/Constants.java
@@ -5,18 +5,20 @@ package jamia.mikko.fallwatch.Detection;
  */
 
 public class Constants {
+    //Define all ACTION strings used in broadcasting and filtering service.
     public interface ACTION {
-        public static String MAIN_ACTION = "jamia.mikko.fallwatch.falldetectionservice.action.main";
-        public static String STARTFOREGROUND_ACTION = "jamia.mikko.fallwatch.falldetectionservice.action.startforeground";
-        public static String STOPFOREGROUND_ACTION = "jamia.mikko.fallwatch.falldetectionservice.action.stopforeground";
-        public static String MESSAGE_RECEIVED = "jamia.mikko.fallwatch.falldetectionservice.action.messagereceived";
-        public static String ALERT_ACTION = "jamia.mikko.fallwatch.falldetectionservice.action.alertaction";
-        public static String YES_ACTION = "jamia.mikko.fallwatch.falldetectionservice.action.yesaction";
-        public static String TIMER_REGISTERED = "jamia.mikko.fallwatch.falldetectionservice.action.timerregistered";
+        String MAIN_ACTION = "jamia.mikko.fallwatch.falldetectionservice.action.main";
+        String STARTFOREGROUND_ACTION = "jamia.mikko.fallwatch.falldetectionservice.action.startforeground";
+        String STOPFOREGROUND_ACTION = "jamia.mikko.fallwatch.falldetectionservice.action.stopforeground";
+        String MESSAGE_RECEIVED = "jamia.mikko.fallwatch.falldetectionservice.action.messagereceived";
+        String ALERT_ACTION = "jamia.mikko.fallwatch.falldetectionservice.action.alertaction";
+        String YES_ACTION = "jamia.mikko.fallwatch.falldetectionservice.action.yesaction";
+        String TIMER_REGISTERED = "jamia.mikko.fallwatch.falldetectionservice.action.timerregistered";
 
     }
 
+    //Foreground service id.
     public interface NOTIFICATION_ID {
-        public static int FOREGROUND_SERVICE = 101;
+        int FOREGROUND_SERVICE = 101;
     }
 }
